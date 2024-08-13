8426efc5ed38bac74de0cc74466ba816686371ec
SagarMore02 <saagarmoree@gmail.com>
2024-08-13 23:13:07 +0530

    Database Queries Done

--- Backend/DatabaseCreation.sql
+++ Backend/DatabaseCreation.sql
@@ -1,9 +1,12 @@
-    CREATE TABLE `SuperUser`(
+CREATE DATABASE IF NOT EXISTS `Questify`;
+    USE `Questify`;
+
+    CREATE TABLE if not exists `SuperUser`(
         `username` varchar(20) NOT NULL,
         `password` varchar(20) NOT NULL
     );
 
-    CREATE TABLE `User_Master` (
+    CREATE TABLE if not exists `User_Master`(
         `userID` int NOT NULL,
         `username` varchar(20) NOT NULL,
         `password` varchar(20) NOT NULL,
@@ -13,27 +16,28 @@
         `mobile` varchar(15) NOT NULL,
         `email` varchar(25) NOT NULL,
         `status` enum('Pending','Active','Inactive') DEFAULT 'Pending',
-        `timestamp` varchar(60) NOT NULL,
+        `timestamp` timestamp NOT NULL,
         PRIMARY KEY (`userID`)
     );
 
-    CREATE TABLE `Exam_Organizer` (
+    CREATE TABLE if not exists `Organization`(
+        `organizationID` int NOT NULL,
+        `name` varchar(10) NOT NULL,
+        `location` varchar(15) NOT NULL,
+        `timestamp` timestamp NOT NULL,
+        FOREIGN KEY (`organizationID`) REFERENCES `User_Master` (`userID`)
+    );
+
+    CREATE TABLE if not exists `Exam_Organizer`(
         `examorganID` int NOT NULL,
         `organizationID` int NOT NULL,
-        `timestamp` varchar(60) NOT NULL,
+        `timestamp` timestamp NOT NULL,
         FOREIGN KEY (`examorganID`) REFERENCES `User_Master` (`userID`),
         FOREIGN KEY (`organizationID`) REFERENCES `Organization` (`organizationID`)
     );
 
-    CREATE TABLE `Organization` (
-        `organizationID` int NOT NULL,
-        `name` varchar(10) NOT NULL,
-        `location` varchar(15) NOT NULL,
-        `timestamp` varchar(60) NOT NULL,
-        FOREIGN KEY (`organizationID`) REFERENCES `User_Master` (`userID`)
-    );
     /*Complete Your Profile To Register For A Test And Approval Of Fees*/
-    CREATE TABLE `Application_Master` (
+    CREATE TABLE if not exists `Application_Master`(
         `applicationID` int NOT NULL,
         `examID` int NOT NULL,
         `adhaarcard` varchar(14) NOT NULL,
@@ -42,13 +46,13 @@
         `appstatus` enum('Pending','Active','Inactive') NOT NULL,
         `attendance` enum('Pending','Present','Absent') NOT NULL,
         `marks` int NOT NULL,
-        `timestamp` varchar(60) NOT NULL,
-        FOREIGN KEY (`applicationID`) REFERENCES `User_Master` (`userID`)
+        `timestamp` timestamp NOT NULL,
+        FOREIGN KEY (`applicationID`) REFERENCES `User_Master` (`userID`),
         PRIMARY KEY (`examID`, `applicationID`)
         
     );
 
-    CREATE TABLE `Exam_Master` (
+    CREATE TABLE if not exists `Exam_Master`(
         `examID` int NOT NULL,
         `organizerID` int NOT NULL,
         `name` varchar(15) NOT NULL,
@@ -62,12 +66,12 @@
         `status` enum('Pending','Completed') NOT NULL,
         `fees` int NOT NULL,
         `syllabus` varchar(100) NOT NULL,
-        `timestamp` varchar(60) NOT NULL,
+        `timestamp` timestamp NOT NULL,
         PRIMARY KEY (`examID`,`organizerID`),
         FOREIGN KEY (`organizerID`) REFERENCES `Exam_Organizer` (`examorganID`)
     );
 
-    CREATE TABLE `Question_Master` (
+    CREATE TABLE if not exists `Question_Master`(
         `questionID` int NOT NULL,
         `examID` int NOT NULL,
         `questiontype` varchar(20) NOT NULL,
@@ -78,12 +82,12 @@
         `optionD` varchar(40) NOT NULL,
         `answer_key` enum('optionA','optionB','optionC','optionD') NOT NULL,
         `question_marks` int NOT NULL,
-        `timestamp` varchar(60) NOT NULL,
+        `timestamp` timestamp NOT NULL,
         PRIMARY KEY (`questionID`, `examID`),
         FOREIGN KEY (`examID`) REFERENCES `Exam_Master` (`examID`)
     );
 
-    CREATE TABLE `Attempt_Master` (
+    CREATE TABLE if not exists `Attempt_Master`(
         `attemptID` int UNIQUE NOT NULL,
         `examID` int NOT NULL,
         `questionID` int NOT NULL,
@@ -91,19 +95,19 @@
         `selected_option` enum('optionA','optionB','optionC','optionD') NOT NULL,
         `correct_option` enum('optionA','optionB','optionC','optionD') NOT NULL,
         `marks_obt` int NOT NULL,
-        `timestamp` varchar(60) NOT NULL,
+        `timestamp` timestamp NOT NULL,
         PRIMARY KEY (`examID`, `questionID`, `applicationID`),
         FOREIGN KEY (`examID`) REFERENCES `Application_Master` (`examID`),
         FOREIGN KEY (`questionID`) REFERENCES `Question_Master` (`questionID`),
         FOREIGN KEY (`applicationID`) REFERENCES `Application_Master` (`applicationID`)
     );
 
-    CREATE TABLE `Transaction_Master` (
+    CREATE TABLE if not exists `Transaction_Master`(
         `trans_id` int NOT NULL,
         `exam_id` int NOT NULL,
         `paidfees` int NOT NULL,
         `upi_token` varchar(40) NOT NULL,
-        `timestamp` varchar(60) NOT NULL,
+        `timestamp` timestamp NOT NULL,
         PRIMARY KEY (`trans_id`),
         FOREIGN KEY (`exam_id`) REFERENCES `Exam_Master` (`examID`)
     );
