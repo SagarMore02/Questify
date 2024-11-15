9a7961c056354854a8a48bbf93d53a7bf12522d6
SagarMore02 <saagarmoree@gmail.com>
2024-11-16 00:04:30 +0530

    'Modal'

--- Backend/Index.js
+++ Backend/Index.js
@@ -67,9 +67,9 @@ app.use(session({
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password:'root',
+  //password:'root',
   //password:'Aditya@123',
-  //password: 'sagar@123',
+  password: 'sagar@123',
   //password:'pr@n@v06',
   //password:'root',
   //password:'101201',
