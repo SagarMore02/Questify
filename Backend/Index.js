153d9112519e15c595d7a16114237602078e73ea
SagarMore02 <saagarmoree@gmail.com>
2024-11-21 15:15:51 +0530

    validations

--- Backend/Index.js
+++ Backend/Index.js
@@ -67,10 +67,10 @@ app.use(session({
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password:'root',
+  //password:'root',
   //password:'Aditya@123',
   //password: 'sagar@123',
-  //password: 'sagar@123',
+  password: 'sagar@123',
   //password: 'sagar@123',
   //password:'pr@n@v06',
   //password:'root',
